e80eeca63d8e7d5426fa0d8c8f910d88b6950b1d
Tyler French <81813288+FrenchMCX@users.noreply.github.com>
2022-05-03 14:03:05 -0400

    projects

--- JavaScript/JavaScript_1.js
+++ JavaScript/JavaScript_1.js
@@ -27,3 +27,24 @@ function Cat_Function() {
     }
     document.getElementById("Output").innerHTML = Cat_Output;
 }
+
+//text change function
+function Change() {
+    var A = document.getElementsByClassName("Click");
+    A[0].innerHTML = "The text has  changed!";
+}
+
+//canvas graphic
+var c = document.getElementById("canvas");
+var ctx = c.getContext("2d");
+ctx.beginPath();
+ctx.arc(95, 50, 40, 0, 2 * Math.PI);
+ctx.stroke();
+
+//gradient for canvas
+var grd = ctx.createLinearGradient(0, 0, 170, 0);
+grd.addColorStop(0, "black");
+grd.addColorStop(1, "white");
+
+ctx.fillStyle = grd;
+ctx.fillRect(0, 0, 200, 100);
